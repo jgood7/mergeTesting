afa44101a7ccf9603b7b2b2a7b0ea8a6a4859bb3
jgood_eGain <jgood@egain.com>
2024-06-26 13:28:15 -0400

    changed i to double

--- TestingFile.java
+++ TestingFile.java
@@ -3,7 +3,7 @@ public class TestingFile{
     public static void main(String[] args) {
         System.out.println("Hello world");
 
-        int i = 1;
+        double i = 1.0; //Changed to double
 
         System.out.println(i);
     }
